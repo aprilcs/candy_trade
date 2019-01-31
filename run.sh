12e11f352818aee18c1ef50dd48b634558e8433d
April <aprilcs@users.noreply.github.com>
2019-01-31 09:51:19 -0500

    Add files via upload

---
+++ run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+python fig1_happiness_of_individuals.py
+python fig2_distribution_of_happiness.py
+
+pip freeze > ../results/requirements_pip.txt
+conda list --export > ../results/requirements_conda.txt
+
+jupyter nbconvert \
+  --ExecutePreprocessor.allow_errors=True \
+  --ExecutePreprocessor.timeout=-1 \
+  --FilesWriter.build_directory=../results \
+  --execute candy_trade.ipynb
